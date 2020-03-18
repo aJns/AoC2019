d81e7fe8f473eefae19d7e9fba5af04fc8460edf
Jonas Nikula <jonas.nikula@gmail.com>
2020-03-18 13:34:23 +0100

    Work on day2

--- day2/day2-1.hs
+++ day2/day2-1.hs
@@ -5,5 +5,16 @@ main = do
     args <- getArgs
     contents <- readFile $ head args
     let program = map read $ splitOn "," contents :: [Int]
-    print program
+    print $ parseProg 0 program
     
+parseProg :: [Int] -> Int -> Int
+parseProg instPtr program
+  | opcode == 1 = parseProg (instPtr + 4) $ add program instPtr
+  | opcode == 2 = parseProg (instPtr + 4) $ mult program instPtr
+  | opcode == 99 = head program
+  | otherwise = error "Invalid opcode"
+  where opcode = program !! instPtr
+
+add :: [Int] -> Int -> [Int]
+
+mult :: [Int] -> Int -> [Int]
